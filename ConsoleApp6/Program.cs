c4b9dd175f8ea121fd2afefec6bdf8dd0181d189
user <user@526-2>
2022-09-14 11:23:00 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lang = "it"; 
+            switch (lang)
+            {
+                case "en":
+                    Console.WriteLine("Hello world!");
+                    break;
+                case "ru":
+                    Console.WriteLine("Привет мир!");
+                    break;
+                case "jpn":
+                    Console.WriteLine("こんにちは世界!");
+                    break;
+                case "it":
+                    Console.WriteLine("Ciao mondo!");
+                    break;
+                default:
+                    Console.WriteLine("Локализация не найдена!");
+                    break;
+            }
+        }
+    }
+}
